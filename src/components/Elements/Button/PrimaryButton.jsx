6c06669a87816a10d02caa03eaef7fd68b99442b
wahyu.esya17 <wahyu.esya17@gmail.com>
2024-03-16 23:23:26 +0700

    feat(button): add rest props

--- src/components/Elements/Button/PrimaryButton.jsx
+++ src/components/Elements/Button/PrimaryButton.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/prop-types */
 const PrimaryButton = (props) => {
-  const { children, classname, type = "submit", onClick = () => {} } = props;
+  const {
+    children,
+    classname,
+    type = "submit",
+    onClick = () => {},
+    ...rest
+  } = props;
   return (
     <button
       type={type}
       onClick={onClick}
       className={`h-10 px-6 font-semibold text-ternary hover:bg-hover-primary transition duration-150 ease-in-out ${classname}`}
+      {...rest}
     >
       {children}
     </button>
--- src/components/Elements/Button/SecondaryButton.jsx
+++ src/components/Elements/Button/SecondaryButton.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 const SecondaryButton = (props) => {
-  const { children, classname, onClick = () => {} } = props;
+  const { children, classname, onClick = () => {}, ...rest } = props;
   return (
     <button
       onClick={onClick}
       className={`h-10 px-6 font-semibold text-primary border border-primary flex items-center justify-center hover:bg-primary hover:text-ternary transition duration-150 ease-in-out ${classname}`}
+      {...rest}
     >
       {children}
     </button>
